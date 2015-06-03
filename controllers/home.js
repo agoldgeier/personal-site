55dac3f111e9544c0ca440228fb4fe72876a7697
Devin Visslailli <devin.viss@nyu.edu>
2015-06-03 17:02:59 -0400

    Initial Template Commit

---
+++ controllers/home.js
@@ -0,0 +1,6 @@
+angular.module('2fCollective')
+  .controller('HomeCtrl', function($scope) {
+
+  	$scope.title = 'Hi there!';
+ 
+  });
---
+++ js/app.js
@@ -0,0 +1,36 @@
+angular.module('2fCollective', ['ngRoute', 'ngMessages'])
+  .config(function($routeProvider, $locationProvider) {
+
+  	// Provides routing
+  	$routeProvider
+		  .when('/', {
+		    templateUrl: 'views/home.html',
+		    controller: 'HomeCtrl'
+		  })
+		  .when('/artists', {
+		    templateUrl: 'views/artists.html',
+		    controller: 'ArtistsCtrl'
+		  })
+		  .when('/albums', {
+		    templateUrl: 'views/albums.html',
+		    controller: 'AlbumsCtrl'
+		  })
+		  .when('/videos', {
+		    templateUrl: 'views/videos.html',
+		    controller: 'VidCtrl'
+		  })
+		  .when('/contact', {
+		    templateUrl: 'views/contact.html',
+		    controller: 'ContactCtrl'
+		  })
+		  .when('/blog', {
+		    templateUrl: 'views/blog.html',
+		    controller: 'BlogCtrl'
+		  })
+		  .otherwise('/');
+
+
+		  // UNCOMMENT TO GET RID OF #'s
+		  // $locationProvider.html5Mode(true);
+ 
+  });
