139091af3abbe71ecec50917239e555ae2234843
agoldgeier <aviv.goldgeier@gmail.com>
2015-06-05 19:13:50 -0400

    Added featured albums on home page

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,23 @@
 angular.module('2fCollective')
-  .controller('HomeCtrl', function($scope, Page) {
+  .controller('HomeCtrl', function($scope, Page, $routeParams, $http) {
 
   	Page.setTitle('Home');
+
+  	$http.get('albums.json').success(function(data) {
+    	$scope.albums = data;
+
+        var featured = ["The River Looks Beautiful", 
+  		"The River Looks Beautiful1", "The River Looks Beautiful2", 
+  		"The River Looks Beautiful3"];
+
+  		$scope.featuredAlbums = {};
+  		for (var i = 0; i < 4; i++) {
+  			$scope.featuredAlbums[featured[i]] = $scope.albums[featured[i]];
+  		}
+      console.log($scope.albums);
+    });
+
+
+
  
   });
