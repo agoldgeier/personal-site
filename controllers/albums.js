12c3e3f4f27cad8c8a1c7d04ff1a06550b6bdf0e
agoldgeier <aviv.goldgeier@gmail.com>
2015-06-05 18:54:55 -0400

    Added albums grid

--- controllers/albums.js
+++ controllers/albums.js
@@ -1,6 +1,15 @@
 angular.module('2fCollective')
-  .controller('AlbumsCtrl', function($scope, Page) {
+  .controller('AlbumsCtrl', function($scope, Page, $routeParams, $http) {
+
+  	$http.get('albums.json').success(function(data) {
+      $scope.albums = data;
+    });
 
   	Page.setTitle('Albums');
  
+  	// THIS IS CALLED AFTER TO UPDATE THE SCOPE
+    function updateInfo() {
+      $scope.$apply();
+    }
+
   });
