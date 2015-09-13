19d25b13c84e925f9d26cb0bd0e51e55b8012bef
agoldgeier <aviv.goldgeier@gmail.com>
2015-09-12 21:10:13 -0400

    Album pages working; still have to adjust artist pages accordingly

--- controllers/album.js
+++ controllers/album.js
@@ -1,5 +1,5 @@
 angular.module('2fCollective')
-  .controller('AlbumCtrl', function($scope, Page, $routeParams, $http) {
+  .controller('AlbumCtrl', function($scope, $sce, Page, $routeParams, $http) {
 
     $scope.init = function(name) {
       $scope.name = name;
@@ -9,6 +9,16 @@ angular.module('2fCollective')
       $scope.album = data[$scope.name];
       // console.log($scope.artist);
     });
+
+    $scope.makeLink = function(name) {
+      return "/#/artists/" + name.toLowerCase().replace(/ /g,'-');
+    }
+
+    $scope.makeEmbed = function(num) {
+      return "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/" + num + "&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true";
+    }
+
+    $scope.trust = $sce.trustAsHtml;
 
   	// THIS IS CALLED AFTER TO UPDATE THE SCOPE
     function updateInfo() {
